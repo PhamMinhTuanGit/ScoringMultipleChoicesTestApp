c0e605acf3a2b4227e197e6e818719da7bbc5327
PhamMinhTuanGit <tuan170804@gmail.com>
2024-12-20 11:15:27 +0700

    test tren tap label thanh cong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fixed_coordinates import fixed_circle
 
-input_image_path = 'Image/sheet_raw.jpg'
+input_image_path = '/Users/phamminhtuan/Desktop/Trainning_SET/Images/IMG_1587_iter_13.jpg'
 output_file = "test.txt"
 
 fixed_circle(input_image_path, output_file)
