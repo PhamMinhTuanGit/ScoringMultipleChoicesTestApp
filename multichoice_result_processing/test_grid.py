aade2d03367f7d54ce982c99d563fa16b2a491da
Blackdier <nguyenkhai_hsgs19@hus.edu.vn>
2024-12-22 18:25:58 +0700

    Warp perspective update

--- multichoice_result_processing/test_grid.py
+++ multichoice_result_processing/test_grid.py
@@ -1,7 +1,7 @@
 import image_processing
 import grid_info
 import visualization
-input_image_path = 'D:\\4_2_AREAS\\testset1\\testset1\\images\\IMG_3960_iter_95.jpg'
+input_image_path = 'D:\\4_2_AREAS\\testset1\\testset1\\images\\IMG_3960_iter_0.jpg'
 #input_data = 'IMG_1581_iter_0.txt'
 result_txt_path = 'results_test_2.txt'
 
@@ -11,7 +11,7 @@ centers = image_processing.getNails(input_image_path)
 #visualization.drawDots(input_image_path,centers,"draw_centers.jpg")
 gridmatrix=grid_info.getGridmatrix(centers)
 print("The length of the grid is: ",)
-# section_grid=grid_info.getExtractsections(gridmatrix)
-# print(section_grid[3][1])
-flaten_matrix=grid_info.flattenMatrix(gridmatrix)
-visualization.drawDots(input_image_path,flaten_matrix,output_path="")
+section_grid=grid_info.getExtractsections(gridmatrix)
+print(section_grid[3][1])
+#flaten_matrix=grid_info.flattenMatrix(gridmatrix)
+visualization.drawDots(input_image_path,section_grid[3][0])
