6b2d6a34993bf0a8ffd96c84b5bd7469585145cc
Blackdier <nguyenkhai_hsgs19@hus.edu.vn>
2024-12-23 09:50:26 +0700

    test grid ok

--- bubble_classify.py
+++ bubble_classify.py
@@ -3,7 +3,6 @@ import math
 import numpy as np
 from sklearn.cluster import DBSCAN
 from utilities import append_to_file
-import torch
 
 def sort_to_convex_quadrilateral(dots):
     """
--- test_grid.py
+++ test_grid.py
@@ -1,7 +1,8 @@
 import image_processing
 import grid_info
 import visualization
-input_image_path = 'D:\\4_2_AREAS\\testset1\\testset1\\images\\IMG_3960_iter_0.jpg'
+#input_image_path = 'D:\\4_2_AREAS\\testset1\\testset1\\images\\IMG_3960_iter_0.jpg'
+input_image_path = "IMG_1581_iter_0.jpg"
 #input_data = 'IMG_1581_iter_0.txt'
 result_txt_path = 'results_test_2.txt'
 
@@ -10,7 +11,7 @@ centers = image_processing.getNails(input_image_path)
 #visualization.drawDots(input_image_path,"draw_centers_duc.jpg")
 #visualization.drawDots(input_image_path,centers,"draw_centers.jpg")
 gridmatrix=grid_info.getGridmatrix(centers)
-print("The length of the grid is: ",)
+print("The length of the grid is: ",len(centers))
 section_grid=grid_info.getExtractsections(gridmatrix)
 print(section_grid[3][1])
 #flaten_matrix=grid_info.flattenMatrix(gridmatrix)
