6d9daddbd6871fae31a1f5d4c5e36edac15de6ca
Blackdier <nguyenkhai_hsgs19@hus.edu.vn>
2024-12-23 09:43:35 +0700

    Change some path on test grid

--- multichoice_result_processing/test_grid.py
+++ multichoice_result_processing/test_grid.py
@@ -1,14 +1,16 @@
 import image_processing
 import grid_info
 import visualization
-input_image_path = 'D:\\4_2_AREAS\\testset1\\testset1\\images\\IMG_3960_iter_0.jpg'
+#input_image_path = 'D:\\4_2_AREAS\\testset1\\testset1\\images\\IMG_3960_iter_0.jpg'
+#input_image_path ="IMG_1581_iter_0.jpg"
+input_image_path="D:\\4_2_AREAS\\Trainning_SET\\Images\\IMG_1581_iter_1.jpg"
 #input_data = 'IMG_1581_iter_0.txt'
 result_txt_path = 'results_test_2.txt'
 
 centers = image_processing.getNails(input_image_path)
 #centers_duc=image_processing.detect_black_square_centers(input_image_path)
 #visualization.drawDots(input_image_path,"draw_centers_duc.jpg")
-#visualization.drawDots(input_image_path,centers,"draw_centers.jpg")
+visualization.drawDots(input_image_path,centers,"draw_centers.jpg")
 gridmatrix=grid_info.getGridmatrix(centers)
 print("The length of the grid is: ",)
 section_grid=grid_info.getExtractsections(gridmatrix)
